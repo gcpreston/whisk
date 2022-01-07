c4ac167ebd811a8e90f43bb35e271196784d9145
Graham Preston <graham.preston@gmail.com>
2022-01-07 17:57:10 -0500

    2x2 and 3x3 functionality

--- lib/whisk.ex
+++ lib/whisk.ex
@@ -1,18 +1,86 @@
 defmodule Whisk do
   @moduledoc """
-  Documentation for `Whisk`.
+  A twisty puzzle scramble generator.
   """
+
+  @typedoc """
+  Supported puzzle types are:
+  - "2x2"
+  - "3x3"
+  """
+  @type puzzle_type :: String.t()
+
+  @typedoc """
+  A scramble is string representing a sequence of moves, separated by spaces.
+
+  Moves are also strings. Different puzzle types have different valid moves.
+  """
+  @type scramble :: String.t()
+
+  # A puzzle spec is a 3-tuple containing the following:
+  # - a list of groups of turns on the same axis (e. g. R and L for cubes)
+  # - a list of turn modifiers (e. g. for cubes, ', 2, and nothing)
+  # - the default move count in a scramble
+
+  @puzzle_spec_2x2 {[~w(R), ~w(F), ~w(U)], ["", "'", "2"], 10}
+  @puzzle_spec_3x3 {[~w(R L), ~w(F B), ~w(U D)], ["", "'", "2"], 20}
+
+  ## API
 
   @doc """
-  Hello world.
+  Generate a scramble for a puzzle type.
 
-  ## Examples
-
-      iex> Whisk.hello()
-      :world
-
+  Passing an unsupported puzzle type will generate an error.
   """
-  def hello do
-    :world
+  @spec scramble(puzzle_type(), list()) :: scramble()
+  def scramble(puzzle_name, opts \\ []) do
+    {axes, modifiers, default_length} = puzzle_spec(puzzle_name)
+    length = opts[:length] || default_length
+
+    generate_scramble(axes, modifiers, length)
+  end
+
+  ## Helpers
+
+  defp puzzle_spec("2x2") do
+    @puzzle_spec_2x2
+  end
+
+  defp puzzle_spec("3x3") do
+    @puzzle_spec_3x3
+  end
+
+  defp puzzle_spec(_) do
+    raise "Unsupported puzzle type"
+  end
+
+  defp generate_scramble(axes, modifiers, length) do
+    axis_idx = :rand.uniform(Enum.count(axes)) - 1
+    Enum.join(generate_moves(axes, modifiers, axis_idx, [], length), " ")
+  end
+
+  defp generate_moves(axes, modifiers, last_axis_index, acc, remaining) do
+    if remaining <= 0 do
+      acc
+    else
+      axis_idx = different_axis_index(axes, last_axis_index)
+
+      generate_moves(
+        axes,
+        modifiers,
+        axis_idx,
+        [generate_move(axes, modifiers, axis_idx) | acc],
+        remaining - 1
+      )
+    end
+  end
+
+  defp generate_move(axes, modifiers, axis_idx) do
+    Enum.random(Enum.at(axes, axis_idx)) <> Enum.random(modifiers)
+  end
+
+  defp different_axis_index(axes, index) do
+    axis_count = Enum.count(axes)
+    rem(index + :rand.uniform(axis_count - 1), axis_count)
   end
 end
--- test/whisk_test.exs
+++ test/whisk_test.exs
@@ -2,7 +2,77 @@ defmodule WhiskTest do
   use ExUnit.Case
   doctest Whisk
 
-  test "greets the world" do
-    assert Whisk.hello() == :world
+  describe "2x2" do
+    @valid_moves ~w(R R' R2 F F' F2 U U' U2)
+
+    test "defaults to length 10" do
+      scramble = Whisk.scramble("2x2")
+      assert Enum.count(String.split(scramble)) == 10
+    end
+
+    test "length can be customized" do
+      scramble = Whisk.scramble("2x2", length: 15)
+      assert Enum.count(String.split(scramble)) == 15
+    end
+
+    test "negative length gives empty scramble" do
+      assert String.length(Whisk.scramble("2x2", length: -1)) == 0
+    end
+
+    test "contains only valid moves" do
+      scramble = Whisk.scramble("2x2")
+      assert Enum.all?(String.split(scramble), fn move -> move in @valid_moves end)
+    end
+
+    test "does not contain subsequent moves on the same axis" do
+      move_groups = [~w(R R' R2), ~w(F F' F2), ~w(U U' U2)]
+
+      move_group_index = fn move ->
+        Enum.find_index(move_groups, fn group -> move in group end)
+      end
+
+      scramble = Whisk.scramble("2x2")
+
+      for [move1, move2] <- Enum.chunk_every(String.split(scramble), 2, 1, :discard) do
+        refute move_group_index.(move1) == move_group_index.(move2)
+      end
+    end
+  end
+
+  describe "3x3" do
+    @valid_moves ~w(R R' R2 L L' L2 F F' F2 B B' B2 U U' U2 D D' D2)
+
+    test "defaults to length 20" do
+      scramble = Whisk.scramble("3x3")
+      assert Enum.count(String.split(scramble)) == 20
+    end
+
+    test "length can be customized" do
+      scramble = Whisk.scramble("3x3", length: 25)
+      assert Enum.count(String.split(scramble)) == 25
+    end
+
+    test "negative length gives empty scramble" do
+      assert String.length(Whisk.scramble("3x3", length: -1)) == 0
+    end
+
+    test "contains only valid moves" do
+      scramble = Whisk.scramble("3x3")
+      assert Enum.all?(String.split(scramble), fn move -> move in @valid_moves end)
+    end
+
+    test "does not contain subsequent moves on the same axis" do
+      move_groups = [~w(R R' R2 L L' L2), ~w(F F' F2 B B' B2), ~w(U U' U2 D D' D2)]
+
+      move_group_index = fn move ->
+        Enum.find_index(move_groups, fn group -> move in group end)
+      end
+
+      scramble = Whisk.scramble("3x3")
+
+      for [move1, move2] <- Enum.chunk_every(String.split(scramble), 2, 1, :discard) do
+        refute move_group_index.(move1) == move_group_index.(move2)
+      end
+    end
   end
 end
